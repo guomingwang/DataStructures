5ce01eaa2ffa0f98222a4e4cd98f2c71865430f1
wangguoming <512191350@qq.com>
2020-05-03 07:56:05 +0800

    实现循环数组队列

--- src/com/learning/queue/CircleArrayQueueDemo.java
+++ src/com/learning/queue/CircleArrayQueueDemo.java
@@ -3,40 +3,48 @@ package com.learning.queue;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayQueueDemo {
+public class CircleArrayQueueDemo {
 
     public static void main(String[] args) {
-        ArrayQueue arrayQueue = new ArrayQueue(3);
+        CircleArrayQueueDemo.CircleArrayQueue circleArrayQueue = new CircleArrayQueueDemo.CircleArrayQueue(4);
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("请输入命令:_");
+            System.out.print("\n请输入命令:_");
             char charAt = scanner.next().charAt(0);
             switch (charAt) {
                 case 'f' :
                     try {
-                        Integer showFront = arrayQueue.showFront();
-                        System.out.println("arrayQueue.showFront: " + showFront);
+                        Integer showFront = circleArrayQueue.showFront();
+                        System.out.println("circleArrayQueue.showFront: " + showFront);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     } finally {
-                        System.out.println(arrayQueue.toString());
+                        System.out.println(circleArrayQueue.toString());
+                        circleArrayQueue.show();
                     }
                     break;
                 case 'a' :
                     System.out.print("请输入数据:_");
                     int nextInt = scanner.nextInt();
-                    arrayQueue.add(nextInt);
-                    System.out.println(arrayQueue.toString());
+                    circleArrayQueue.add(nextInt);
+                    System.out.println(circleArrayQueue.toString());
+                    circleArrayQueue.show();
                     break;
                 case 'g' :
                     try {
-                        Integer get = arrayQueue.get();
-                        System.out.println("arrayQueue.get " + get);
+                        Integer get = circleArrayQueue.get();
+                        System.out.println("circleArrayQueue.get " + get);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     } finally {
-                        System.out.println(arrayQueue.toString());
+                        System.out.println(circleArrayQueue.toString());
+                        circleArrayQueue.show();
                     }
+                    break;
+                case 'l' :
+                    Integer length = circleArrayQueue.length();
+                    System.out.println("circleArrayQueue.length: " + length);
+                    circleArrayQueue.show();
                     break;
                 default:
                     break;
@@ -44,21 +52,21 @@ public class ArrayQueueDemo {
         }
     }
 
-    private static class ArrayQueue {
+    private static class CircleArrayQueue {
         private Integer size;
+        private Integer[] array;
         private Integer front;
         private Integer rear;
-        private Integer[] array;
 
-        public ArrayQueue(Integer size) {
+        public CircleArrayQueue(Integer size) {
             this.size = size;
-            this.front = -1;
-            this.rear = -1;
             this.array = new Integer[this.size];
+            this.front = 0;
+            this.rear = 0;
         }
 
         public boolean isFull() {
-            return rear == size - 1;
+            return (rear + 1) % size == front;
         }
 
         public boolean isEmpty() {
@@ -70,15 +78,17 @@ public class ArrayQueueDemo {
                 System.out.println("队列已满，不能加数据~");
                 return;
             }
-            array[++rear] = add;
+            array[rear] = add;
+            rear = (rear + 1) % size;
         }
 
         public Integer get() throws Exception {
             if (isEmpty()) {
                 throw new Exception("队列已空，不能取数据~");
             }
-            Integer get = array[++front];
+            Integer get = array[front];
             array[front] = null;
+            front = (front + 1) % size;
             return get;
         }
 
@@ -86,7 +96,25 @@ public class ArrayQueueDemo {
             if (isEmpty()) {
                 throw new Exception("队列已空，不能查看头部数据~");
             }
-            return array[front + 1];
+            return array[front];
+        }
+
+        public void show() {
+            if (isEmpty()) {
+                System.out.println("队列已空，不能查看数据~");
+                return;
+            }
+            for (int i = 0; i < length(); i++) {
+                int index = (front + i) % size;
+                System.out.printf("array[%d] = %d\n", index, array[index]);
+            }
+        }
+
+        public Integer length() {
+            if (isEmpty()) {
+                return 0;
+            }
+            return (rear + size - front) % size;
         }
 
         @Override
