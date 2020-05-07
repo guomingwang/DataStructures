30aa1dcc5c4f3d0b573acc3b359f9be336b27da0
wangguoming <512191350@qq.com>
2020-05-08 02:02:15 +0800

    希尔排序

---
+++ src/com/learning/sort/ShellSort.java
@@ -0,0 +1,36 @@
+package com.learning.sort;
+
+import java.util.Arrays;
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        System.out.println(Arrays.toString(shellSort(array)));
+    }
+
+    private static int[] shellSort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+        // 10/2=5; 5/2=2; 2/2=1
+        // 分5组；分2组；分1组
+        int multiple = 2;
+        int groups = array.length / multiple;
+        while (groups >= 1) {
+            for (int i = groups; i < array.length; i += groups) {
+                int temp = array[i];
+                int j = i - groups;
+                while (j >= 0 && array[j] > temp) {
+                    array[j + groups] = array[j];
+                    j -= groups;
+                }
+                j += groups;
+                array[j] = temp;
+            }
+            System.out.println(Arrays.toString(array));
+            groups /= multiple;
+        }
+        return array;
+    }
+}
